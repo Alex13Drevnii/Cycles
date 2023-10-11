96dac9322a08b2dc33f19df02f3a30dad78e80b6
Alexandr Kuznetsov <kyznecov_sasha@list.ru>
2023-10-11 12:55:30 +0300

    Add cycle

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+            int count = 0; //Количество найденных путей
+            System.out.println("Введите путь к файлу:");
+            String path = new Scanner(System.in).nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+
+            boolean isDirectory = file.isDirectory();
+
+            if (isDirectory) System.out.println("Путь ведет к папке");
+            else if (!fileExists) System.out.println("Файла не существует");
+            else {
+                count++;
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер " + count);
+            }
+
+        }
+        }
+}
